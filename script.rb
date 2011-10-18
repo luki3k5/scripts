ec52218793bf5be4734aab306658b276b3fbdd0c
luki3k5 <luki3k5@gmail.com>
2011-10-18 12:32:00 +0200

    additional changes on the script to parse the doc and create YMLs with translations

--- script.rb
+++ script.rb
@@ -16,23 +16,44 @@ session = GoogleSpreadsheet.login(login, password)
 # getting the first worksheet 
 ws = session.spreadsheet_by_key("0Anp_bn4ib4f7dEo5R1FSSWdCTFdLbGs4cnlETnFVNHc").worksheets[0]
 
-# -1 column because the first on would be keys
-no_of_languages = ws.num_cols-1
+# -2 columns because the first on would be keys and second one is description
+no_of_languages = ws.num_cols-2
 
-languages = []
-(2..ws.num_cols).each do |c|
-  languages << ws[1,c]
+languages = {}
+(3..ws.num_cols).each do |c|
+  languages["#{ws[1,c]}"] = c
 end
 
-puts languages.class 
-puts languages.inspect
+# puts "I have following languages in columns: #{languages.inspect}"
 
+languages.keys.each do |lang| 
+  # create a file: 
+  f = File.open(lang, 'w+')
+  # drop content to file from the keys and corresponding lang:
+  (2..ws.num_rows).each do |row|
+    f.puts [ws[row, 1], ws[row, languages[lang]]].join(":")
+  end
+  f.close
+end
 
-# (1..ws.num_rows).each do |row|
-#   (1..ws.num_cols).each do |col|
-#     puts ws[row, col]
+# i18n_file = {}
+# (2..ws.num_rows).each do |r|
+#   languages.keys.each do |lang_key|
+#      i18n_file["#{ws[r,1]}"] = ws[r, languages[lang_key]]
 #   end
 # end
 
-# #puts ws.rows
+
+# puts i18n_file.inspect
+# # puts languages.class 
+# # puts languages.inspect
+
+
+# # (1..ws.num_rows).each do |row|
+# #   (1..ws.num_cols).each do |col|
+# #     puts ws[row, col]
+# #   end
+# # end
+
+# # #puts ws.rows
 
